175696ae6a60723c0070c5ef90ec4cb49bee4825
Pascalx777 <jesusbeba74@gmail.com>
2024-10-08 21:01:15 -0400

    Add files via upload

---
+++ anim.js
@@ -0,0 +1,42 @@
+
+var audio = document.querySelector("audio");
+var lyrics = document.querySelector("#lyrics");
+
+
+
+// Animar las letras
+function updateLyrics() {
+  var time = Math.floor(audio.currentTime);
+  var currentLine = lyricsData.find(
+    (line) => time >= line.time && time < line.time + 6
+  );
+
+  if (currentLine) {
+    
+    var fadeInDuration = 0.1; 
+    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
+
+    // Aplica el efecto de aparición
+    lyrics.style.opacity = opacity;
+    lyrics.innerHTML = currentLine.text;
+  } else {
+   
+    lyrics.style.opacity = 0;
+    lyrics.innerHTML = "";
+  }
+}
+
+setInterval(updateLyrics, 1000);
+
+
+function ocultarTitulo() {
+  var titulo = document.querySelector(".titulo");
+  titulo.style.animation =
+    "fadeOut 3s ease-in-out forwards"; 
+  setTimeout(function () {
+    titulo.style.display = "none";
+  }, 3000); 
+}
+
+
+setTimeout(ocultarTitulo, 216000);
